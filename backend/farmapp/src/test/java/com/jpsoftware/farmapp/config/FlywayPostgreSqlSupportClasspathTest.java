38d8d8cf96f2122d2d2baca85751419e3918a914
JUAN-P-A-B-L-O <juanpablo25102001@gmail.com>
2026-05-07 03:46:37 -0300

    test(ai): update tests for copilation-bugs-fix

--- backend/farmapp/src/test/java/com/jpsoftware/farmapp/config/FlywayPostgreSqlSupportClasspathTest.java
+++ backend/farmapp/src/test/java/com/jpsoftware/farmapp/config/FlywayPostgreSqlSupportClasspathTest.java
@@ -2,6 +2,9 @@ package com.jpsoftware.farmapp.config;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.flywaydb.core.api.configuration.ClassicConfiguration;
+import org.flywaydb.core.internal.database.DatabaseType;
+import org.flywaydb.core.internal.database.DatabaseTypeRegister;
 import org.flywaydb.database.postgresql.PostgreSQLConfigurationExtension;
 import org.flywaydb.database.postgresql.PostgreSQLDatabaseType;
 import org.junit.jupiter.api.Test;
@@ -10,11 +13,17 @@ class FlywayPostgreSqlSupportClasspathTest {
 
     @Test
     void postgresqlFlywayModuleIsAvailableOnClasspath() {
-        PostgreSQLDatabaseType databaseType = new PostgreSQLDatabaseType();
-        PostgreSQLConfigurationExtension configurationExtension = new PostgreSQLConfigurationExtension();
+        ClassicConfiguration configuration = new ClassicConfiguration();
+        DatabaseType databaseType = DatabaseTypeRegister.getDatabaseTypeForUrl(
+                "jdbc:postgresql://localhost:5432/farmdb",
+                configuration);
+        PostgreSQLConfigurationExtension configurationExtension =
+                configuration.getConfigurationExtension(PostgreSQLConfigurationExtension.class);
 
+        assertThat(databaseType).isInstanceOf(PostgreSQLDatabaseType.class);
         assertThat(databaseType.getName()).isEqualTo("PostgreSQL");
         assertThat(databaseType.handlesJDBCUrl("jdbc:postgresql://localhost:5432/farmdb")).isTrue();
+        assertThat(configurationExtension).isNotNull();
         assertThat(configurationExtension.getNamespace()).isEqualTo("postgresql");
     }
 }
